a1eb3133558508b46876af529f12c43e21982f50
eliceoflmov72 <eliceomoreta@gmail.com>
2024-03-01 15:37:23 +0100

    Servicios añaddidos

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
+// Es importante que este componente HTTP sea importado en la configuración
+// El servicio podrá consultar el tipo de rutas con el protocolo http
+// Cunado se van a invocar apis y servicios siempre se tiene que exportar este componente
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),provideHttpClient()]
 };
